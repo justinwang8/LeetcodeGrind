b5fd8057f952260e75b12c69fff3fb45a8a8b490
Justin Wang <64336791+justinwang8@users.noreply.github.com>
2023-05-06 18:03:15 -0400

    May 6

---
+++ 746. Min Cost Climbing Stairs/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+
+//Time: O(n)
+//Space: O(1)
+
+var minCostClimbingStairs = function(cost) {
+    cost.push(0);
+
+    for(let i = (cost.length - 3); i >= 0; i--){
+        cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+    }
+
+    return Math.min(cost[0], cost[1]);
+
+};
